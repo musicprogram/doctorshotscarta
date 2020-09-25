03b5561abbb33d1c0ef789619cde2c8d51c26a99
musicprogram <jahenao17@misena.edu.co>
2020-09-25 15:50:26 -0500

    data

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 const regletas =[
   { id: 1,
     name: 'Tequila Sunrise',
-    value: '$15.000',
+    value: '$17.000',
     value2: '',
     description: '6 tragos de tequila con jugo de naranja licor de durazno y granadina.'
   },
@@ -26,6 +26,62 @@ const regletas =[
     value: '$15.000',
     value2: '',
     description: '6 tragos de vodka con ginebra y curazao azul.'
+  },
+  {
+    id: 1,
+    name: 'Cocaína rusa',
+    value: '$15.000',
+    value2: '',
+    description: '6 tragos de vodka con limón, café y azúcar.'
+  },
+  {
+    id: 1,
+    name: 'Cocaína Kofla',
+    value: '$15.000',
+    value2: '',
+    description: '6 tragos de vodka, ginebra con limón y sal.'
+  },
+  {
+    id: 1,
+    name: 'Lithium',
+    value: '$17.000',
+    value2: '',
+    description: '6 tragos con tequila, whisky, ginebra y amaretto.'
+  },
+  {
+    id: 1,
+    name: 'Gin Pepper',
+    value: '$15.000',
+    value2: '',
+    description: '6 tragos de vodka, ginebra con limón, pimienta y azúcar.'
+  },
+  {
+    id: 1,
+    name: 'Jaggeranja',
+    value: '$25.000',
+    value2: '',
+    description: ''
+  },
+  {
+    id: 1,
+    name: 'Jagger',
+    value: '$35.000',
+    value2: '',
+    description: ''
+  },
+  {
+    id: 1,
+    name: 'Jack Daniels',
+    value: '$40.000',
+    value2: '',
+    description: ''
+  },
+  {
+    id: 1,
+    name: 'Jagger Bomb',
+    value: '$20.000',
+    value2: '',
+    description: '6 tragos con Jager y energizante.'
   }
 ];
 
@@ -35,16 +91,79 @@ const cocktails =[
   {
     id: 2,
     name: 'Tequila Sunrise',
-    value: '$28.000',
-    value2: '$12.000',
+    value: '$15.000',
+    value2: '$25.000',
     description: 'Tequila con jugo de naranja licor de durazno y granadina.'
   },
   {
     id: 2,
     name: 'Laguna Azul',
-    value: '$25.000',
-    value2: '$10.000',
+    value: '$15.000',
+    value2: '$20.000',
     description: 'Vodka con limón y curazao azul.'
+  },
+  {
+    id: 2,
+    name: 'Bitch Mango',
+    value: '$15.000',
+    value2: '$20.000',
+    description: 'Vodka con ginebra limón y mango biche.'
+  },
+  {
+    id: 2,
+    name: 'Mojito',
+    value: '$17.000',
+    value2: '$25.000',
+    description: 'Ron con azúcar, hierbabuena y limón. '
+  },
+  {
+    id: 2,
+    name: 'Monstruo',
+    value: '$13.000',
+    value2: '$20.000',
+    description: ''
+  },
+  {
+    id: 2,
+    name: 'Jack and Jagger',
+    value: '$20.000',
+    value2: '$40.000',
+    description: 'Jack Daniel´s con jager y energizante o soda.'
+  },
+  {
+    id: 2,
+    name: 'Jagger Bomb',
+    value: '$18.000',
+    value2: '$45.000',
+    description: 'Jager, energizante o soda.'
+  },
+  {
+    id: 2,
+    name: 'Ultravioleta',
+    value: '$15.000',
+    value2: '$20.000',
+    description: 'Vodka, ginebra curazao y granadina.'
+  },
+  {
+    id: 2,
+    name: 'Red Machine',
+    value: '$15.000',
+    value2: '$20.000',
+    description: 'Vodka, limón , granadina y gaseosa.'
+  },
+  {
+    id: 2,
+    name: 'Jaggeranja',
+    value: '$18.000',
+    value2: '$35.000',
+    description: ''
+  },
+  {
+    id: 2,
+    name: 'Cuba Libre',
+    value: '$15.000',
+    value2: '$20.000',
+    description: ''
   }
 ];
 
@@ -53,22 +172,78 @@ const beers =[
     id: 3,
     name: 'Amarilla',
     value: '$6.000',
-    value2: '$27.000',
+    value2: '',
     description: 'Cerveza con limón tequila y naranja.'
   },
   {
     id: 3,
-    name: 'Napolitana',
-    value: '$7.000',
-    value2: '$20.000',
-    description: 'Cerveza con jugo de tomate estilo Bloody Mary y tabasco.'
+    name: 'Roja',
+    value: '$6.000',
+    value2: '',
+    description: 'Cerveza con limón y fresa.'
   },
   {
     id: 3,
     name: 'Verde',
     value: '$6.000',
-    value2: '$27.000',
+    value2: '',
     description: 'Cerveza con curazao y limón.'
+  },
+  {
+    id: 3,
+    name: 'Asesina',
+    value: '$7.000',
+    value2: '',
+    description: 'Cerveza con vodka, ginebra, ron blanco.'
+  },
+  {
+    id: 3,
+    name: 'Clorophila',
+    value: '$7.000',
+    value2: '',
+    description: 'Cerveza con jager y limón.'
+  },
+  {
+    id: 3,
+    name: 'Michelada',
+    value: '$5.000',
+    value2: '',
+    description: 'Cerveza con limón y sal.'
+  },
+  {
+    id: 3,
+    name: 'Cubana',
+    value: '$7.000',
+    value2: '',
+    description: 'Cerveza con tabasco.'
+  },
+  {
+    id: 3,
+    name: 'Nacionales',
+    value: '$4.000',
+    value2: '',
+    description: ''
+  },
+  {
+    id: 3,
+    name: 'Club Colombia',
+    value: '$5.000',
+    value2: '',
+    description: ''
+  },
+  {
+    id: 3,
+    name: 'Importadas',
+    value: '$7.000',
+    value2: '',
+    description: ''
+  },
+  {
+    id: 3,
+    name: 'Corona',
+    value: '$8.000',
+    value2: '',
+    description: ''
   }
 ];
 
@@ -120,16 +295,16 @@ function list(arrayCocktails){
         <h6 class="mt-1 text-right font-weight-bold text-carta-value">
                
           ${shot.id === 1 ? '' + shot.value : ''}
-          ${shot.id === 2 ? 'Litro: ' + shot.value : ''}
-          ${shot.id === 3 ? 'Jarra: ' + shot.value : ''}
+          ${shot.id === 2 ? 'Vaso: ' + shot.value : ''}
+          ${shot.id === 3 ? 'Vaso: ' + shot.value : ''}
                 
         </h6>
    
         <h6 class="mt-1 text-right font-weight-bold text-carta-value">
        
           ${shot.id === 1 ? '' + shot.value2 : ''}
-          ${shot.id === 2 ? 'Vaso: ' + shot.value2 : ''}
-          ${shot.id === 3 ? 'Vaso: ' + shot.value2 : ''}
+          ${shot.id === 2 ? 'Litro: ' + shot.value2 : ''}
+          ${shot.id === 3 ? '' + shot.value2 : ''}
         
     
         </h6>
